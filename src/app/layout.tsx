cad3e1ad040b177d17374552422f4b0ea1a8b4bb
Starboy <samskywalker001@gmail.com>
2026-01-18 14:35:47 +0000

    light theme by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
+          // defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
