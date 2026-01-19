73237009395b738d5e3bfb835a17e282f67259fa
Starboy <samskywalker001@gmail.com>
2026-01-19 04:25:20 +0000

    text-shadow adjustments

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   return (
     <main className="via-primary flex flex-col items-center justify-center bg-gradient-to-b from-transparent to-transparent">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="font-paprika text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl text-shadow-xl">
+        <h1 className="font-paprika text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl text-shadow-3xl">
           Starboy<span className="text-primary">&apos;</span>s Projects
         </h1>
       </div>
@@ -18,7 +18,7 @@ export default function HomePage() {
       <br />
       <hr />
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="font-paprika text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl text-shadow-xl">
+        <h1 className="font-paprika text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl text-shadow-3xl">
           Active Projects<span className="text-primary">!</span>
         </h1>
       </div>
@@ -26,7 +26,7 @@ export default function HomePage() {
       <br />
       <hr />
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="font-paprika text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl text-shadow-xl">
+        <h1 className="font-paprika text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl text-shadow-3xl">
           Ex<span className="text-primary">-</span>Projects
         </h1>
       </div>
@@ -34,7 +34,7 @@ export default function HomePage() {
       <br />
       <hr />
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="font-paprika text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl text-shadow-xl">
+        <h1 className="font-paprika text-3xl font-bold tracking-tight md:text-4xl lg:text-5xl xl:text-6xl text-shadow-3xl">
           Archives<span className="text-primary">.</span>
         </h1>
       </div>
